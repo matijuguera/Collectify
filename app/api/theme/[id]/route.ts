6a64010d68e2929b0a5078f69bcc2595f552d328
Matias Juguera <mati.juguera@gmail.com>
2025-02-05 13:58:54 -0300

    fix them get id

---
+++ app/api/theme/[id]/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import { PrismaThemeRepository } from "../../repositories/Theme";
+import ThemeService from "../../services/ThemeService";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const themeService = new ThemeService(new PrismaThemeRepository());
+  const theme = await themeService.get(id);
+  if (!theme) {
+    return NextResponse.json({}, { status: 404 });
+  }
+  return NextResponse.json(theme);
+}
